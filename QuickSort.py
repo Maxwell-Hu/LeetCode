cb1ac4aaecac5afd6e81a787e2a89a102e7a771d
max <huzehua@outlook.com>
2017-08-10 11:30:50 +0800

    An elegent solution for Quick Sort

--- QuickSort.py
+++ QuickSort.py
@@ -1,6 +1,6 @@
 def Qsort(nums):
     if len(nums) <= 1:
         return nums
-    l = [x for x in nums[1:] if x < nums[0]]
+    l = [x for x in nums[1:] if x <= nums[0]]
     r = [x for x in nums[1:] if x > nums[0]]
     return Qsort(l) + [nums[0]] + Qsort(r)
