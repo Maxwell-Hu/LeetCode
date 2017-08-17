a9ed53a608fc676ccc2b74a059537d2153bcb767
max <huzehua@outlook.com>
2017-08-17 17:20:43 +0800

    simplify original solution

--- SimplifyPath.py
+++ SimplifyPath.py
@@ -4,11 +4,7 @@ class Solution(object):
         :type path: str
         :rtype: str
         """
-        dirname_list = path.split('/')
-        while '' in dirname_list:
-            dirname_list.remove('')
-        while '.' in dirname_list:
-            dirname_list.remove('.')
+        dirname_list = [dirname for dirname in path.split('/') if dirname != '' and dirname != ',']
         while '..' in dirname_list:
             idx = dirname_list.index('..')
             dirname_list.remove(dirname_list[idx])
